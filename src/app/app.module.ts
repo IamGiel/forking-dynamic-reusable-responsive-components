2de060b44730a83f2fb038500769b4aa19362a13
IamGiel <november14giel@gmail.com>
2020-03-06 10:46:02 -0500

    my responsive UI study

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {RoutingModule} from './routing/routing.module';
+import {MatToolbarModule} from '@angular/material';
+import {HomeComponent} from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    /* Angular Material Modules */
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RoutingModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
